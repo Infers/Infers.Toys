cdc4e923e9732ceee984da552c13e627b7f41446
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-28 15:40:31 +0300

    Just Infers.Toys

---
+++ Infers.Toys.fsx
@@ -0,0 +1,13 @@
+#I "Libs/Infers.Toys/bin/Debug"
+
+#r "Infers.dll"
+#r "Infers.Rep.dll"
+#r "PPrint.dll"
+#r "Infers.Toys.dll"
+
+open PPrint
+open Infers.Toys
+open Infers.Rep
+open Infers
+
+#time "on"
--- play.sh
+++ play.sh
@@ -3,7 +3,7 @@
 ./build.sh Debug
 
 if hash fsharpi &> /dev/null ; then
-  rlwrap -t dumb fsharpi --use:Infers.fsx
+  rlwrap -t dumb fsharpi --use:Infers.Toys.fsx
 else
-  rlwrap -t dumb fsianycpu.exe --use:Infers.fsx
+  rlwrap -t dumb fsianycpu.exe --use:Infers.Toys.fsx
 fi
